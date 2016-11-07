3d2a0a35a4d8961aa01321e53eff779680c5e38b
ifalko <falko.ftk@gmail.com>
2016-11-07 21:28:50 +0300

    Added configuration file

---
+++ config.py
@@ -0,0 +1,19 @@
+s__author__ = 'falko'
+
+import datetime
+import time
+
+USER_API_KEY ="7gYU7nTl2hUcjXRc32gLbg(("
+#"0UF5ACUj99QXbyg3etLrHw(("
+#"Cl)hcXzXYRYeT5BG479)GA(("
+
+HOST = 'localhost'
+
+QUEUE_NAME = "iFalko"
+
+IMPOSE_THROTTLING = True
+THROTTLE_STOP = False
+
+FROM_DATE = time.mktime(datetime.datetime(2016, 9, 24, 0, 0).timetuple())
+
+TO_DATE = time.mktime(datetime.datetime(2016, 9, 26, 0, 0).timetuple())
